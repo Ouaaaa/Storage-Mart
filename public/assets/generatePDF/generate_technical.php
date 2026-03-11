79ad5848aa950f2e1c601da435347c20ee05ed5f
Josh Ricafort <rj.ricafort21@gmail.com>
2026-03-11 22:27:35 +0800

    Update generate_technical.php

--- public/assets/generatePDF/generate_technical.php
+++ public/assets/generatePDF/generate_technical.php
@@ -1,6 +1,5 @@
-
-
 <?php
+ob_start();
 require '../vendor/autoload.php';
 require_once "config.php";
 include('session-checker.php');
@@ -13,54 +12,38 @@ if (!isset($_GET['ticket_id'])) {
 $ticket_id = intval($_GET['ticket_id']);
 
 $sql = "
-
 SELECT 
     t.ticket_number,
     t.concern_details,
     t.priority,
     t.date_filed,
     t.status,
-
     e.firstname AS emp_firstname,
     e.middlename AS emp_middlename,
     e.lastname AS emp_lastname,
     e.position AS emp_position,
-
     b.branchName,
-
     tt.technical_purpose,
     tt.action_taken,
     tt.result,
     tt.remarks,
     tt.date_performed,
-
     it.firstname AS it_firstname,
     it.lastname AS it_lastname
-
 FROM tbltickets t
-
-JOIN tblemployee e 
-    ON t.employee_id = e.employee_id
-
-JOIN tblbranch b 
-    ON e.branch_id = b.branch_id
-
+JOIN tblemployee e ON t.employee_id = e.employee_id
+JOIN tblbranch b ON e.branch_id = b.branch_id
 JOIN (
     SELECT ticket_id, MAX(date_performed) AS max_date
     FROM tblticket_technical
     GROUP BY ticket_id
 ) x ON x.ticket_id = t.ticket_id
-
 JOIN tblticket_technical tt
     ON tt.ticket_id = x.ticket_id
    AND tt.date_performed = x.max_date
-
-JOIN tblemployee it 
-    ON tt.performed_by = it.employee_id
-
+JOIN tblemployee it ON tt.performed_by = it.employee_id
 WHERE t.ticket_id = ?
 ";
-
 
 $stmt = mysqli_prepare($link, $sql);
 mysqli_stmt_bind_param($stmt, "i", $ticket_id);
@@ -83,8 +66,6 @@ if (!file_exists($templatePath)) {
 }
 
 $template = new TemplateProcessor($templatePath);
-
-// XML-safe replacements
 $template->setValue('date_filed', htmlspecialchars($date_filed));
 $template->setValue('fullname', htmlspecialchars($fullname));
 $template->setValue('branchName', htmlspecialchars($data['branchName']));
@@ -98,8 +79,7 @@ $template->setValue('date_performed', htmlspecialchars($date_performed));
 $outputFile = __DIR__ . "/Technical_Report_" . preg_replace('/[^A-Za-z0-9_-]/', '', $data['ticket_number']) . ".docx";
 $template->saveAs($outputFile);
 
-// Clean output buffers before sending
-if (ob_get_length()) ob_end_clean();
+ob_end_clean();
 header("Content-Description: File Transfer");
 header("Content-Disposition: attachment; filename=" . basename($outputFile));
 header("Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document");
